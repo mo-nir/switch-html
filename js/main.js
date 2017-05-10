d7bbcbdc033a881cb53cc6fa8d595535d2bb2f36
Monir <monir.rj@gmail.com>
2017-05-10 20:53:48 +0600

    completed testimonial and happy client area

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 'use strict';
     
     
-    // imagesLoaded
-    $('#container').imagesLoaded( function() {
+//    // imagesLoaded
+//    $('#container').imagesLoaded( function() {
         
         $('.our-lt-wrk-filter-butn-group').on( 'click', 'button', function() {
             var filterValue = $(this).attr('data-filter');
@@ -18,10 +18,51 @@
             columnWidth: '.grid-item'
             }
         })
+        
+//    });
     
     
-    
+    //Owl Carousel Activation for Testimonial Are
+    var owl_testimonial = $('.testimonial-carousel.owl-carousel');
+    owl_testimonial.owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:1800,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
     });
+    
+        // Stop Carousel On MouseEnter
+        $(".testimonial-carousel-item").mouseenter(function(){
+            owl_testimonial.trigger('stop.owl.autoplay')
+        });
+
+        // Start Carousel On MouseLeave
+        $(".testimonial-carousel-item").mouseleave(function(){
+            owl_testimonial.trigger('play.owl.autoplay')
+        });    
+    
+    
+    //Owl Carousel Activation for Testimonial Are
+    var owl_hpy = $('.our-hpy-clnt-carousel.owl-carousel');
+    owl_hpy.owlCarousel({
+        items:4,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:1500,
+    });
+    
+        // Stop Carousel On MouseEnter
+        $(".our-hpy-clnt-carousel-item").mouseenter(function(){
+            owl_hpy.trigger('stop.owl.autoplay')
+        });
+
+        // Start Carousel On MouseLeave
+        $(".our-hpy-clnt-carousel-item").mouseleave(function(){
+            owl_hpy.trigger('play.owl.autoplay')
+        });
+    
     
     
     
